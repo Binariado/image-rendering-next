17f82522c3c0cd8224dbae351dad9fb6a7c468f2
binariado <binariado@gmail.com>
2021-02-18 16:12:14 -0500

    config redux

--- state/store.prod.ts
+++ state/store.prod.ts
@@ -1,18 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
-import { persistStore } from 'redux-persist'
-import thunk from 'redux-thunk';
+import { createStore } from 'redux';
 import { combinedReducers } from './reducers';
 
-const store = createStore(
-  combinedReducers,
-  applyMiddleware(thunk)
+export const store = createStore(
+  combinedReducers
 )
 
-const persistor = persistStore(store);
-
-const configureStore =  {
-  store,
-  persistor
+ const configureStore =  {
+  store
 }
 
 export default configureStore
